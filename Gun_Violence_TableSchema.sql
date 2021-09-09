2385af150f68768db70c6f9eda39797785f598bd
marieprosper77 <85536042+marieprosper77@users.noreply.github.com>
2021-09-08 20:56:09 -0400

    Add files via upload

---
+++ Gun_Violence_TableSchema.sql
@@ -0,0 +1,58 @@
+﻿
+
+CREATE TABLE "2016_gun_violence" (
+    "State" varchar(50)   NOT NULL,
+    "n_killed" int   NOT NULL,
+    "n_injured" int   NOT NULL,
+    CONSTRAINT "pk_2016_gun_violence" PRIMARY KEY (
+        "State"
+     )
+);
+
+CREATE TABLE "2017_gun_violence" (
+    "State" varchar(50)   NOT NULL,
+    "n_killed" int   NOT NULL,
+    "n_injured" int   NOT NULL,
+    CONSTRAINT "pk_2017_gun_violence" PRIMARY KEY (
+        "State"
+     )
+);
+
+CREATE TABLE "2016poverty" (
+    "Location" varchar(50)   NOT NULL,
+    "White" decimal,
+    "Black" decimal,
+    "Hispanic" decimal,
+    "Asian/Native Hawaiian and Pacific Islander" decimal,
+    "American Indian/Alaska Native" decimal,
+    "Multiple Races" decimal,
+    "Total" decimal,
+    CONSTRAINT "pk_2016poverty" PRIMARY KEY (
+        "Location"
+     )
+);
+
+
+CREATE TABLE "2017poverty" (
+    "Location" varchar(50)   NOT NULL,
+    "White" decimal,
+    "Black" decimal,
+    "Hispanic" decimal,
+    "Asian/Native Hawaiian and Pacific Islander" decimal,
+    "American Indian/Alaska Native" decimal,
+    "Multiple Races" decimal,
+    "Total" decimal,
+    CONSTRAINT "pk_2017poverty" PRIMARY KEY (
+        "Location"
+     )
+);
+
+CREATE TABLE "totalpopulation" (
+    "rank" int   NOT NULL,
+    "state" varchar(50)   NOT NULL,
+    "population" int   NOT NULL,
+    "density_per_sq_mi" decimal,
+    CONSTRAINT "population" PRIMARY KEY (
+        "state"
+     )
+);
